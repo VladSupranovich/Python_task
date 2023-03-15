6b150505c04a1a8db1f2100ed158f492f265fb8d
VladSupranovich <BQiXaGrbB@km6bF>
2023-03-15 17:46:32 +0300

    python task v2

--- sql.py
+++ sql.py
@@ -6,6 +6,7 @@ rooms_number_of_students = "SELECT rooms.""name"" AS room_name, count(students.i
 							GROUP BY room_name \
 							ORDER BY room_name"
 
+
 #5 комнат, где самый маленький средний возраст студентов
 rooms_avg_age = "SELECT r.""name"" AS room_name, avg(age(current_date, s.birthday::date)) AS avg_age\
 					FROM rooms r\
@@ -15,6 +16,7 @@ rooms_avg_age = "SELECT r.""name"" AS room_name, avg(age(current_date, s.birthda
 					ORDER BY avg_age\
 					LIMIT 5"
 
+
 #5 комнат с самой большой разницей в возрасте студентов
 rooms_diff_age = "SELECT DISTINCT r.""name"" AS room_name,\
 					max(s.birthday) OVER(PARTITION BY r.""name"") - min(s.birthday) OVER(PARTITION BY r.""name"") AS age_diff\
@@ -23,6 +25,7 @@ rooms_diff_age = "SELECT DISTINCT r.""name"" AS room_name,\
 					ON r.id = s.room\
 					ORDER BY age_diff DESC\
 					LIMIT 5"
+
 
 #Список комнат где живут разнополые студенты
 rooms_diff_sex = "SELECT r.""name"" AS room_name\
